db6223d8dd18149476d121610b0f789936f4a1e8
shivam chauhan <shivam@primafelicitas.com>
2024-08-07 14:38:18 +0530

    chanessss

--- routes/staff/adminRouter.js
+++ routes/staff/adminRouter.js
@@ -1,57 +1,57 @@
-// const express = require("express");
-// const adminRouter = express.Router();
-
-
-// adminRouter.post("/",(req,res)=>{
-//     try{
-//         res.status(201).json({
-//             status: "Success",
-//             data:"Admin has been registered"
-//         });
-//     }catch(error){
-//         res.json({
-//             status: "Failed",
-//             error: error.message,
-//         })
-//     }
-// })
-
-
-// module.exports = adminRouter;
-
-
-
-
-
-
-
-
-
-const express = require('express');
-const { unpublishTeacher, publishingExam, unwithdrawTeacher, withdrawTeacher, unsuspendTeacher, suspendTeacher, deleteAdmin, registerAdminController, updateAdmin, getSingleAdmin, loginAdminController, getAllAdmins } = require('../../controller/staff/adminController')
+const express = require("express");
 const adminRouter = express.Router();
-//register
-adminRouter.post("/register", registerAdminController);
-//login
-adminRouter.post("/login", loginAdminController);
-//get all admins
-adminRouter.get("/", getAllAdmins);
-//get single admin
-adminRouter.get("/:id", getSingleAdmin);
-//update admin
-adminRouter.put("/:id", updateAdmin);
-//delete admin
-adminRouter.delete("/:id", deleteAdmin);
-//admin suspending teacher
-adminRouter.put("/suspend/teacher/:id", suspendTeacher);
-//admin Unsuspending teacher
-adminRouter.put("/unsuspend/teacher/:id", unsuspendTeacher);
-//admin withdrawing teacher
-adminRouter.put("/withdraw/teacher/:id", withdrawTeacher);
-//admin unWithdraw teacher
-adminRouter.put("/unwithdraw/teacher/:id", unwithdrawTeacher);
-//admin publishing exam results
-adminRouter.put("/publish/exam/:id", publishingExam)
-//admin unpublishing exam results
-adminRouter.put("/unpublish/exam/:id", unpublishTeacher)
+
+
+adminRouter.post("/",(req,res)=>{
+    try{
+        res.status(201).json({
+            status: "Success",
+            data:"Admin has been registered"
+        });
+    }catch(error){
+        res.json({
+            status: "Failed",
+            error: error.message,
+        })
+    }
+})
+
+
 module.exports = adminRouter;
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const { unpublishTeacher, publishingExam, unwithdrawTeacher, withdrawTeacher, unsuspendTeacher, suspendTeacher, deleteAdmin, registerAdminController, updateAdmin, getSingleAdmin, loginAdminController, getAllAdmins } = require('../../controller/staff/adminController')
+// const adminRouter = express.Router();
+// //register
+// adminRouter.post("/register", registerAdminController);
+// //login
+// adminRouter.post("/login", loginAdminController);
+// //get all admins
+// adminRouter.get("/", getAllAdmins);
+// //get single admin
+// adminRouter.get("/:id", getSingleAdmin);
+// //update admin
+// adminRouter.put("/:id", updateAdmin);
+// //delete admin
+// adminRouter.delete("/:id", deleteAdmin);
+// //admin suspending teacher
+// adminRouter.put("/suspend/teacher/:id", suspendTeacher);
+// //admin Unsuspending teacher
+// adminRouter.put("/unsuspend/teacher/:id", unsuspendTeacher);
+// //admin withdrawing teacher
+// adminRouter.put("/withdraw/teacher/:id", withdrawTeacher);
+// //admin unWithdraw teacher
+// adminRouter.put("/unwithdraw/teacher/:id", unwithdrawTeacher);
+// //admin publishing exam results
+// adminRouter.put("/publish/exam/:id", publishingExam)
+// //admin unpublishing exam results
+// adminRouter.put("/unpublish/exam/:id", unpublishTeacher)
+// module.exports = adminRouter;
